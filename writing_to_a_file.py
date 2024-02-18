c28958bd623c66f1a4b26df07213c505173bf7dd
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-18 21:40:29 +0800

    pseudo code and txt

---
+++ writing_to_a_file.py
@@ -0,0 +1,8 @@
+# DAY 7: Writing to a File:
+# Create a simple script that asks the user for their name, then writes it to a file called "user_info.txt."
+
+# ========= PSEUDO CODE =========
+# - Code for 'open'
+# - Variable for asking the user's name
+# - Code for displaying the inputted information
+# - Code for closing the file
